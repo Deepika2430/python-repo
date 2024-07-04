4938e90178e16b635317af78b17cc028b3ad04fd
Deepika Pabbisetti <deepika_pabbisetti@tecnics.com>
2024-07-04 19:00:17 +0530

    Removed database_url

--- db.py
+++ db.py
@@ -1,7 +1,5 @@
 import psycopg2
 
-database_url = "dbname=dsp host=192.168.1.140 port=7000 user=postgres password=postgres"
-
 def get_connection_object(connection_string):
     return psycopg2.connect(connection_string)
 
