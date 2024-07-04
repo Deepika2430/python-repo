c83ef1cdd317516d75bceec4cc2772f115ccfb42
Deepika Pabbisetti <deepika_pabbisetti@tecnics.com>
2024-07-04 18:59:06 +0530

    Removed unnecessary print statement

--- filters.py
+++ filters.py
@@ -5,7 +5,6 @@ def filter_pgdb(playlist_item, connection_string, query_config):
     query = get_db_query(query_config)
     response = get_db_response(query, connection_string)
     json_response = get_formatted_response(query_config["columns"], response, query_config["is_array"])
-    print(json_response)
     playlist_item = add_attribute(playlist_item, query_config["target_key"], json_response)
     return playlist_item
 
